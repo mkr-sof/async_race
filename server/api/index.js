49f8012da8a70cfab9269b73b934c16362a533d3
mkr-sof <mkrtchyan_sofia@mail.ru>
2026-06-22 13:05:16 +0400

    fix vercel serverless setup

--- server/api/index.js
+++ server/api/index.js
@@ -41,6 +41,13 @@ const PORT = process.env.PORT || 4000;
 const state = { velocity: {}, blocked: {} };
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 const STATUS = {
     STARTED: 'started',
