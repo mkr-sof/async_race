bcfee64fe8945150ae7ebf174c7a5f290c93fb84
mkr-sof <mkrtchyan_sofia@mail.ru>
2026-06-22 13:15:08 +0400

    fix vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/async_race/',
   plugins: [react()],
   server: { port: 3000 },
   build: { outDir: 'build' },
